80494ad4c625813c8689feecdbc76b5247fa4ca8
Pramod kumar <pramodkumarp6@gmail.com>
2022-03-22 14:30:59 +0530

    git inital commit

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const userRouter = require("./api/users/user.router");
+
+
+
+app.use(express.json());
+
+
+
+
+
+app.use("/api/users", userRouter);
+const port = process.env.PORT || 9000;
+app.listen(port, () => {
+  console.log("server up and running on PORT :", port);
+});
